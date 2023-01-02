2e51e8cba3981ac2f4a34dd0b1ecb62ab8ebe0b3
Gabriel-682 <gabriel.682681@gmail.com>
2023-01-02 16:47:29 -0300

    Test Pass II

--- Pruebas.js
+++ Pruebas.js
@@ -1,6 +1,30 @@
-var objeto = {
-    nombre : "Fernando",
-    apellido : "Enrique"
-};
+  var array = [1, 2, 3, 4, 20, 25, 30];
+  var array2 = [1, 1, 1, 1, 1];
+  var array3 = ["hola", "cómo", "andás?", "andrea"]
+  var numero = 9801;
+  var texto = "fernando"
+  var cont = 0;
 
-console.log(`${objeto.nombre} ${objeto.apellido}`);
+  var objeto1 = {
+    nombre : "Fer",
+    email : "@nada",
+    premium : false
+  };
+ 
+  var objeto2 = {
+    nombre : "Fer",
+    email : "@nada"
+  };
+
+ array.sort((a, b) => {
+  if (a < b) {
+    return -1;
+  }
+  if (a > b) {
+    return 1;
+  }
+  return 0;
+ });
+
+
+  console.log(array[array.length - 1] - array[0]);
